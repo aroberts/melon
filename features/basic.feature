a3bb341c21041325391f946896c56441388d64e6
aroberts <adroberts@gmail.com>
2011-01-24 17:14:01 -0500

    prevented hashing of directories (will add -r later).  added --force option to add

--- features/basic.feature
+++ features/basic.feature
@@ -18,9 +18,16 @@ Feature: Basic usage
     """
     melon: path already present in database
     """
+    And the exit status should not be 0
 
   Scenario: Checking a file that is not in the database
     When I run "melon -d test.db check test_file"
     Then the output should contain "test_file"
     And the output should start with "/"
+
+  Scenario: Adding a directory
+    Given a directory named "testo"
+    When I run "melon -d test.db add testo"
+    Then the output should contain "directory"
+    And the exit status should not be 0
 
--- lib/melon/commands.rb
+++ lib/melon/commands.rb
@@ -10,16 +10,60 @@ module Melon
   module Commands
     # needs a 'verify' command to check integrity of database
     # needs a 'remove' command, or some way to deal with deletes/renames
-    class Add
-      # TODO force option
+    class Base
       attr_accessor :args, :options
+      attr_reader :description
 
       def initialize(args, options)
         self.args = args
         self.options = options
       end
 
+      def parser
+        raise
+      end
+
+      def description
+        raise
+      end
+
+      def parse_options!
+        begin
+          parser.parse!(args)
+        rescue OptionParser::ParseError => e
+          CLI.error "#{self.class.to_s.split("::").last.downcase}: #{e}"
+        end
+      end
+
+    end
+
+    class Add < Base
+
+      def description
+        "Add files to the melon database"
+      end
+
+      def parser
+        @parser ||= OptionParser.new do |p|
+          p.banner = "Usage: melon add file [file [file ...]]"
+          p.separator ""
+          p.separator description
+
+          p.separator ""
+          p.separator "Options:"
+          p.separator ""
+
+          p.on("-f", "--force",
+               "Add a file even if it already exists, overwriting",
+               "any previously stored data") do
+            options.force = true
+               end
+        end
+      end
+
       def run
+        parse_options!
+
         options.database.transaction do
           args.each do |filename|
             # puts "Adding #{filename}..."
@@ -27,15 +71,18 @@ module Melon
             # hash strategy should be encapsulated, ergo indirection here
             hash = Hasher.digest(filename)
 
-            absolute_path = File.expand_path(filename)
+            filename = File.expand_path(filename)
+            if File.directory?(filename)
+              CLI.error "argument is a directory: #{filename}"
+            end
 
-            if options.database[:by_path][absolute_path]:
+            if options.database[:by_path][filename] and !options.force
               CLI.error "path already present in database"
             end
 
-            options.database[:by_hash][hash] = absolute_path
-            options.database[:by_path][absolute_path] = hash
-            puts "#{hash}:#{absolute_path}"
+            options.database[:by_hash][hash] = filename
+            options.database[:by_path][filename] = hash
+            puts "#{hash}:#{filename}"
           end
         end
       end
