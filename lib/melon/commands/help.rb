3cca97ff94953ba46ea45ddf83666c2f0feaf7d9
Andrew Roberts <aroberts@letterbox.local>
2011-01-28 01:54:11 -0500

    fixed help

--- lib/melon/commands/help.rb
+++ lib/melon/commands/help.rb
@@ -1,10 +1,22 @@
 require 'melon/commands/base'
+require 'melon/commands'
 
 module Melon
   module Commands
     class Help < Base
       def self.description
         "Get help with a specific command"
+      end
+
+      def run
+        help = args.shift
+        begin
+          puts Commands[help.capitalize].new(args, options).parser
+        rescue NameError => e
+          # don't swallow NoMethodErrors
+          raise e unless e.instance_of?(NameError)
+          error "unrecognized command: #{help}"
+        end
       end
     end
   end
