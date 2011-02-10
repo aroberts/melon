aa5f539afe92eeea0377fd2880b9737a9915d541
aroberts <adroberts@gmail.com>
2011-02-10 14:19:49 -0500

    refactored a bunch of stuff in cli

--- lib/melon/cli.rb
+++ lib/melon/cli.rb
@@ -14,7 +14,7 @@ module Melon
       new(arguments).run
     end
 
-    attr_accessor :arguments
+    attr_accessor :arguments, :options, :parser 
 
     def initialize(arguments)
       self.arguments = arguments
@@ -26,40 +26,8 @@ module Melon
       options
     end    
 
-    def run
-      options = parse_options
-      options.database = PStore.new(File.expand_path(options.database_path))
-
-      # prepare db
-      options.database.transaction do
-        options.database[:by_hash] ||= {}
-        options.database[:by_path] ||= {}
-      end
-
-      unless arguments.empty?
-        run_command(options)
-      end
-    end
-
-    def run_command(options)
-      # look for command class in args.shift
-      command_name = arguments.shift
-      begin
-        c = Commands[command_name.capitalize]
-      rescue NameError => e
-        # don't swallow NoMethodErrors
-        raise e unless e.instance_of?(NameError)
-        error "unrecognized command: #{command_name}"
-      end
-      c.new(arguments, options).run
-    end
-
-    def parse_options
-      options = self.class.default_options
-
-      # TODO: empty args should be -h
-
-      parser = OptionParser.new do |p|
+    def parser
+      @parser ||= OptionParser.new do |p|
         p.banner = "Usage: melon [options] COMMAND [command-options] [ARGS]"
 
         p.separator ""
@@ -67,7 +35,6 @@ module Melon
         p.separator ""
 
         Commands.each do |command|
-          # help goes last
           if command.command_name == 'help'
             next
           end
@@ -75,9 +42,6 @@ module Melon
           p.separator format_command(command.command_name,
                                      command.description)
         end
-        # TODO: add help command back into parser helptext
-        # p.separator format_command(Commands::Help.command_name,
-        #                            Commands::Help.description)
         p.separator ""
         p.separator "Options:"
         p.separator ""
@@ -87,16 +51,60 @@ module Melon
           options.database_path = database
              end
 
-        p.on_tail("-v", "--version", "Show version") do
+        p.on("-v", "--version", "Show version") do
           puts Melon.version_string
           exit 0
         end
 
-        p.on_tail("-h", "--help", "This is it") do
+        p.on("-h", "--help", "This is it") do
           puts p
           exit 0
         end
+
+        p.separator ""
+        p.separator "For more information on a specific command," +
+          " use 'melon help COMMAND'"
       end
+
+    end
+
+    def run
+      parse_options
+      options.database = PStore.new(File.expand_path(options.database_path))
+
+      # prepare db
+      options.database.transaction do
+        options.database[:by_hash] ||= {}
+        options.database[:by_path] ||= {}
+      end
+
+      if arguments.empty?
+        puts parser
+        exit
+      else
+        # look for command class in args.shift
+        command_name = arguments.shift
+
+        run_command(command_name)
+      end
+    end
+
+    def run_command(command_name)
+      begin
+        command_class = Commands[command_name.capitalize]
+      rescue NameError => e
+        # don't swallow NoMethodErrors
+        raise e unless e.instance_of?(NameError)
+        error "unrecognized command: #{command_name}"
+      end
+      c = command_class.new(arguments, options)
+      (command_name == 'help' ? c.run(self) : c.run)
+    end
+
+    def parse_options
+      self.options = self.class.default_options
+
+      # TODO: empty args should be -h
 
       begin
         parser.order!(arguments)
